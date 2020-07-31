203371f3a57f7bfca902da101ddf7e77879db6a9
chrisPiemonte <chris.piemonte.nj@gmail.com>
2020-08-01 00:11:47 +0200

    minor changes

--- app.py
+++ app.py
@@ -37,6 +37,6 @@ def ui():
 
 
 if __name__ == '__main__':
-    # port = int(os.environ.get('PORT', 5000))
-    # app.run(host='0.0.0.0', port=port)
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run(host='0.0.0.0')
